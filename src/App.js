463a6b883bf6e3ae617ded1032d064185709c321
Gustavo Denardi Merlugo <gmerlugodenardi@gmail.com>
2024-04-19 14:51:47 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const ThreeSquares = () => {
+  return (
+    <div className="container">
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+    </div>
+  );
+};
+
+export default ThreeSquares;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ThreeSquares from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThreeSquares />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
