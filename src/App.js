f606da3ba2702dc4cd507708c99da74c2b68bf23
julioSCassol <065201@urisantiago.br>
2024-04-20 18:15:25 -0300

    Header V1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import './App.css';
-=======
 import Header from './Header/header';
 import Home from './home/home.js'; 
->>>>>>> Stashed changes
 
 const ThreeSquares = () => {
   return (
-<<<<<<< Updated upstream
-    <div className="container">
-      <div className="square"></div>
-      <div className="square"></div>
-      <div className="square"></div>
-=======
     <div>
       <Header />
       <Home />
->>>>>>> Stashed changes
     </div>
   );
 };
