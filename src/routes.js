94cb2c38868ff723b3d66e5083838cedd3d515aa
Gustavo Denardi Merlugo <gmerlugodenardi@gmail.com>
2024-04-28 15:42:00 -0300

    Melhora na responsividade e criação de rotas

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,18 @@ import { Routes, Route } from 'react-router-dom';
 
 import LoginPage from './LoginPage/login';
 import HomePage from './home/home'; 
+import VideosPage from './Videos/videos'; 
+import MaterialsPage from './Materials/materials'; 
+import ChallengesPage from './Challenges/challenges'; 
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/videos" element={<VideosPage />} />
+            <Route path="/materials" element={<MaterialsPage />}/>
+            <Route path="/challenges" element={<ChallengesPage />}/>
         </Routes>
     );
 };
