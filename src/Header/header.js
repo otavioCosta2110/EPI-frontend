edf6fce83ae9f6e4d10adf54ea9742db4d44817a
Gustavo Denardi Merlugo <gmerlugodenardi@gmail.com>
2024-05-15 12:53:13 -0300

    fix: fixed header positions

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,12 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faArrowRightFromBracket,
-} from "@fortawesome/free-solid-svg-icons";
+import { faUser, faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import "./header.css";
-import { handleLogout } from "../LoginPage/login";
 import Cookies from "js-cookie";
 
 function Header({ style }) {
@@ -28,45 +24,49 @@ function Header({ style }) {
 
   return (
     <header className="header" style={style}>
-      {user ? (
-        <div className="user-info">
-          {user.data.role === "0" ? (
-            <Link to="/adminPage">
+      <div className="header-column left-section">
+        {user ? (
+          <div className="user-info">
+            {user.data.role === "0" ? (
+              <Link to="/adminPage">
+                <span>{user.data.name}</span>
+              </Link>
+            ) : (
               <span>{user.data.name}</span>
+            )}
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+        ) : (
+          <div className="login">
+            <Link to="/login">
+              <span>Login/Registro</span>
             </Link>
-          ) : (
-            <span>{user.data.name}</span>
-          )}
-          <FontAwesomeIcon icon={faUser} />
-        </div>
-      ) : (
-        <div className="login">
-          <Link to="/login">
-            <span>Login/Registro</span>
-          </Link>
-          <FontAwesomeIcon icon={faUser} />
-        </div>
-      )}
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+        )}
+      </div>
 
-      <div className="logo-container">
+      <div className="header-column center-section">
         <Link to="/">
           <h1>EPI</h1>
         </Link>
       </div>
-      
-      {user && (
-        <div className="Logout" onClick={handleLogout}>
-          <Link to="/">
-            <h3>
-              <FontAwesomeIcon
-                icon={faArrowRightFromBracket}
-                style={{ marginRight: "0.5rem" }}
-              />
-              Sair
-            </h3>
-          </Link>
-        </div>
-      )}
+
+      <div className="header-column right-section">
+        {user && (
+          <div className="Logout" onClick={handleLogout}>
+            <Link to="/">
+              <h3>
+                <FontAwesomeIcon
+                  icon={faArrowRightFromBracket}
+                  style={{ marginRight: "0.5rem" }}
+                />
+                Sair
+              </h3>
+            </Link>
+          </div>
+        )}
+      </div>
     </header>
   );
 }
