91dfe704eb790323ca37b6c1372a4735bfb749cb
julioSCassol <065201@urisantiago.br>
2024-05-17 14:45:48 -0300

    Feat: Página de Videos

---
+++ src/AdminPage/Videos/VideosRegister.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import { TextField } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
+import Cookies from 'js-cookie';
+
+function VideoRegister() {
+    const [title, setTitle] = useState('');
+    const [url, setUrl] = useState('');
+    const [description, setDescription] = useState('');
+    const [error, setError] = useState('');
+    const [tags, setTags] = useState([]);
+    const [selectedTags, setSelectedTags] = useState([]);
+    const [adminId, setAdminId] = useState('');
+    const [user, setUser] = useState('');
+
+    const apiURL = 'http://localhost:3000';
+
+    useEffect(() => {
+        const fetchTags = async () => {
+            try {
+                const tagResponse = await fetch(`${apiURL}/tag/gettags`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+                const tagsData = await tagResponse.json();
+                const tagNames = tagsData.data.map(tag => tag.name);
+                setTags(tagNames);
+            } catch (error) {
+                console.error('Error fetching tags:', error);
+            }
+        };
+
+        fetchTags();
+    }, []);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+          setUser(JSON.parse(storedUser));
+        }
+      }, []);
+
+    useEffect(() => {
+        if (user && user.data && user.data.id) {
+        setAdminId(user.data.id);
+        const AdminId = user.data.id;
+        }
+      }, [user]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`${apiURL}/video/create`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                title,
+                url,
+                description,
+                tags: selectedTags,
+                user_id: adminId,
+              }),
+            });
+            if (response.ok) {
+                window.location.href = "/adminPage";
+            } else {
+                setError("Erro ao registrar vídeo");
+            }
+            if (selectedTags.length > 0) {
+                tags = selectedTags;
+            }
+        }catch (error) {
+            setError("Erro ao registrar vídeo");
+        } 
+    };
+    
+    
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Título:
+                <input
+                    type="text"
+                    name="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)} />
+            </label>
+            <label>
+                URL do vídeo:
+                <input
+                    type="text"
+                    name="url"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)} />
+            </label>
+            <label>
+                Descrição:
+                <textarea
+                    name="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)} />
+            </label>
+            <label>
+                Tags:
+                <Autocomplete
+                    multiple
+                    id="combo-box-demo"
+                    options={tags}
+                    className="autocomplete-container"
+                    renderInput={(params) => <TextField {...params} className="autocomplete-input" label="Tags" />}
+                    onChange={(event, newValue) => {
+                        setSelectedTags(newValue);
+                    }}
+                />
+            </label>
+            <input type="submit" value="Registrar Vídeo" />
+            {error && <div className="error">{error}</div>}
+        </form>
+    );
+}
+
+export default VideoRegister;
--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,11 @@ import HomePage from './home/home';
 import AdminPage from './AdminPage/adminPage';
 import RegisterPage from './RegisterPage/register';
 import RegisterStudent from './RegisterStudent/register';
-import VideosPage from './Videos/videos'; 
+import VideosPage from './Videos/Videos'; 
 import MaterialsPage from './Materials/materials'; 
 import ChallengesPage from './Challenges/challenges';
-import JsPage from './Videos/js/jsPage'; 
-import HtmlPage from './Videos/html/htmlPage'; 
-import CssPage from './Videos/css/cssPage'; 
+import VideoPage from './Videos/VideoPage';
+import VideoRegister from './AdminPage/Videos/VideosRegister';
 
 const AppRoutes = () => {
     return (
@@ -19,13 +18,12 @@ const AppRoutes = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/adminPage" element={<AdminPage />} />
+            <Route path="/registerVideo" element={<VideoRegister />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/videos" element={<VideosPage />} />
             <Route path="/materials" element={<MaterialsPage />}/>
             <Route path="/challenges" element={<ChallengesPage />}/>
-            <Route path="/Videos/js" element={<JsPage />}/>
-            <Route path="/Videos/html" element={<HtmlPage />}/>
-            <Route path="/Videos/css" element={<CssPage />}/>
+            <Route path="/videos/:id" element={<VideoPage />}/>
             <Route path="/registerStudent" element={<RegisterStudent />} />
         </Routes>
     );
